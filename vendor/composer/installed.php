7b0597efa420054961b13d0b5f77ab0f07a5c642
Hanan Shafiq <superlogic.dev@gmail.com>
2023-03-03 02:16:34 +0500

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'myfatoorah/laravel-package',
+        'pretty_version' => '2.1.0',
+        'version' => '2.1.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'myfatoorah/laravel-package' => array(
+            'pretty_version' => '2.1.0',
+            'version' => '2.1.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'myfatoorah/library' => array(
+            'pretty_version' => '2.1.1',
+            'version' => '2.1.1.0',
+            'reference' => 'd763db358985fd3cba689a428f0245e9a2e70073',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../myfatoorah/library',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
